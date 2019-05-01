419c0a0c050e62ac28baf27ee4f5c38215f817b0
gyugyu90 <kh2000park@gmail.com>
2019-05-01 11:24:19 +0900

    1.5.1 오브젝트 팩토리를 이용한 스프링 IoC

--- src/main/java/user/dao/DaoFactory.java
+++ src/main/java/user/dao/DaoFactory.java
@@ -1,20 +1,28 @@
 package user.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class DaoFactory {
 
+    @Bean
     public UserDao userDao() {
         return new UserDao(connectionMaker()); // 팩토리 메소드에서 생성 로직을 결정
     }
 
+    @Bean
     public AccountDao accountDao() {
         return new AccountDao(connectionMaker());
     }
 
+    @Bean
     public MessageDao messageDao() {
         return new MessageDao(connectionMaker());
     }
 
-    private ConnectionMaker connectionMaker() {
+    @Bean
+    public ConnectionMaker connectionMaker() {
         return new DConnectionMaker();
     }
 
--- src/test/java/com/gyugyu/tobyspringexample/UserDaoTest.java
+++ src/test/java/com/gyugyu/tobyspringexample/UserDaoTest.java
@@ -1,5 +1,7 @@
 package com.gyugyu.tobyspringexample;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import user.dao.DaoFactory;
 import user.dao.UserDao;
 
@@ -8,7 +10,10 @@ import java.sql.SQLException;
 public class UserDaoTest {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        UserDao dao = new DaoFactory().userDao();
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        UserDao dao = context.getBean("userDao", UserDao.class);
         System.out.println(dao.get("whiteship").getName());
 
     }
