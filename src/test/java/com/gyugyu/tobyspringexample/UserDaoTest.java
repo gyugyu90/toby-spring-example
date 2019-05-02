ba4058ec554b2047d9939f61187a526052eb2d4b
gyugyu90 <kh2000park@gmail.com>
2019-05-02 16:24:39 +0900

    1.8.2 XML을 이용하는 애플리케이션 컨텍스트

--- src/test/java/com/gyugyu/tobyspringexample/UserDaoTest.java
+++ src/test/java/com/gyugyu/tobyspringexample/UserDaoTest.java
@@ -2,6 +2,8 @@ package com.gyugyu.tobyspringexample;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import user.dao.CountingConnectionMaker;
 import user.dao.DaoFactory;
 import user.dao.UserDao;
@@ -12,13 +14,14 @@ public class UserDaoTest {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+//        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
 
         UserDao dao = context.getBean("userDao", UserDao.class);
         System.out.println(dao.get("whiteship").getName());
 
-        CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
-        System.out.println("Connection counter : " + ccm.getCounter());
+//        CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
+//        System.out.println("Connection counter : " + ccm.getCounter());
     }
 
 }
