88851e68b8d5a5f27bb022f4e4c692cf6df1a911
gyugyu90 <kh2000park@gmail.com>
2019-05-03 09:58:51 +0900

    2.2.1 테스트 검증의 자동화

--- src/test/java/com/gyugyu/tobyspringexample/UserDaoTest.java
+++ src/test/java/com/gyugyu/tobyspringexample/UserDaoTest.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.support.GenericXmlApplicationContext;
 import user.dao.DaoFactory;
 import user.dao.UserDao;
+import user.domain.User;
 
 import java.sql.SQLException;
+import java.util.UUID;
 
 public class UserDaoTest {
 
@@ -17,6 +19,25 @@ public class UserDaoTest {
 
         UserDao dao = context.getBean("userDao", UserDao.class);
         System.out.println(dao.get("whiteship").getName());
+
+        User user1 = new User();
+        String uuid = UUID.randomUUID().toString().substring(0,10);
+        user1.setId(uuid);
+        user1.setName("규규");
+        user1.setPassword("password");
+
+        dao.add(user1);
+
+        User user2 = dao.get(uuid);
+
+        if(!user1.getName().equals(user2.getName())) {
+            System.out.println("테스트 실패 (name)");
+        } else if(!user1.getPassword().equals(user2.getPassword())) {
+            System.out.println("테스트 실패 (password)");
+        } else {
+            System.out.println("조회 테스트 성공");
+        }
+
     }
 
 }
