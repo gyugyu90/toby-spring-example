d87dd26d93047ce53bf5bd52079594d7331df9ae
gyugyu90 <kh2000park@gmail.com>
2019-05-04 09:26:15 +0900

    2.5.2 학습 테스트 예제 - 2

--- src/test/java/com/gyugyu/tobyspringexample/JUnitTest.java
+++ src/test/java/com/gyugyu/tobyspringexample/JUnitTest.java
@@ -2,31 +2,32 @@ package com.gyugyu.tobyspringexample;
 
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.sameInstance;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
 public class JUnitTest {
 
-    static JUnitTest testObject;
+    static Set<JUnitTest> testObjects = new HashSet<>();
 
     @Test
     public void test1(){
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 
     @Test
     public void test2(){
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 
     @Test
     public void test3(){
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 
 }
