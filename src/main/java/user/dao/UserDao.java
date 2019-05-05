06fcfeaba8c9f4c7c8a2501a0576dc47522def6b
gyugyu90 <kh2000park@gmail.com>
2019-05-05 16:16:03 +0900

    3.4.1 JdbcContext의 분리

--- src/main/java/user/dao/UserDao.java
+++ src/main/java/user/dao/UserDao.java
@@ -12,15 +12,20 @@ import java.sql.SQLException;
 
 public class UserDao {
 
-    @Autowired
     private DataSource dataSource;
+
+    private JdbcContext jdbcContext;
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
+    public void setJdbcContext(JdbcContext jdbcContext) {
+        this.jdbcContext = jdbcContext;
+    }
+
     public void add(final User user) throws SQLException {
-        jdbcContextWithStatementStrategy(c -> {
+        jdbcContext.workWithStatementStrategy(c -> {
             PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
             // 로컬 클래스의 코드에서 user 접근 가능
             ps.setString(1, user.getId());
@@ -56,7 +61,7 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException {
-        jdbcContextWithStatementStrategy(c -> c.prepareStatement("delete from users")); // 컨텍스트 호출.. 전략 오브젝트 전달
+        jdbcContext.workWithStatementStrategy(c -> c.prepareStatement("delete from users")); // 컨텍스트 호출.. 전략 오브젝트 전달
     }
 
     public int getCount() throws SQLException {
@@ -80,22 +85,6 @@ public class UserDao {
         }
     }
 
-    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c = dataSource.getConnection();
-            ps = stmt.makePreparedStatement(c);
-
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null) { try { ps.close(); } catch (SQLException e) { } }
-            if (c != null) { try { c.close(); } catch (SQLException e) { } }
-        }
-    }
 
 
 }
