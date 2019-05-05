af662774cb3fe24bbb417aa4373660976762ab9c
gyugyu90 <kh2000park@gmail.com>
2019-05-05 15:38:01 +0900

    3.2.2 분리와 재사용을 위한 디자인 패턴 적용

--- src/main/java/user/dao/UserDao.java
+++ src/main/java/user/dao/UserDao.java
@@ -63,35 +63,10 @@ public class UserDao {
 
     public void deleteAll() throws SQLException {
 
-        Connection c = null;
-        PreparedStatement ps = null;
+        StatementStrategy st = new DeleteAllStatement(); // 선정한 전략 클래스의 오브젝트 생성
+        jdbcContextWithStatementStrategy(st); // 컨텍스트 호출.. 전략 오브젝트 전달
+        // 이렇게 하면 모든 statement 마다 오브젝트를 하나씩 만들어야 한다
 
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("delete from users");
-
-            ps.executeUpdate();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-
-            if(ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {
-
-                }
-            }
-
-            if(c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {
-
-                }
-            }
-
-        }
     }
 
     public int getCount() throws SQLException {
@@ -114,5 +89,23 @@ public class UserDao {
             if (c != null) { try { c.close(); } catch (SQLException e) { } }
         }
     }
+
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = dataSource.getConnection();
+            ps = stmt.makePreparedStatement(c);
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null) { try { ps.close(); } catch (SQLException e) { } }
+            if (c != null) { try { c.close(); } catch (SQLException e) { } }
+        }
+    }
+
 
 }
