79ad72ab01482d394eb7f96c0ad9164951f95e9d
gyugyu90 <kh2000park@gmail.com>
2019-05-05 19:15:26 +0900

    3.5.3 템플릿/콜백의 응용 - 템플릿/콜백 재설계

--- src/main/java/learningtest/template/Calculator.java
+++ src/main/java/learningtest/template/Calculator.java
@@ -7,36 +7,25 @@ import java.io.IOException;
 public class Calculator {
 
     public Integer calcSum(String filepath) throws IOException {
-
-        BufferedReaderCallback sumCallback = br -> {
-            Integer sum = 0;
-            String line;
-            while((line = br.readLine()) != null) {
-                sum += Integer.valueOf(line);
-            }
-            return sum;
-        };
-        return fileReadTemplate(filepath, sumCallback);
+        LineCallback sumCallback = (line, value) -> value + Integer.valueOf(line);
+        return lineReadTemplate(filepath, sumCallback, 0);
     }
 
     public Integer calcMultiply(String filepath) throws IOException {
-        BufferedReaderCallback multiplyCallback = br -> {
-            Integer multiply = 1;
-            String line;
-            while((line = br.readLine()) != null) {
-                multiply *= Integer.valueOf(line);
-            }
-            return multiply;
-        };
-        return fileReadTemplate(filepath, multiplyCallback);
+        LineCallback multiplyCallback = (line, value) -> value * Integer.valueOf(line);
+        return lineReadTemplate(filepath, multiplyCallback, 1);
     }
 
-    private Integer fileReadTemplate(String filepath, BufferedReaderCallback callback) throws IOException {
+    public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal) throws IOException {
         BufferedReader br = null;
         try {
             br = new BufferedReader(new FileReader(filepath));
-            // 콜백 오브젝트 호출 템플릿에서 만든 컨텍스트 정보인 BufferedReader를 전달해주고 콜백의 작업결과를 받아온다.
-            return callback.doSomethingWithReader(br);
+            Integer res = initVal;
+            String line;
+            while((line = br.readLine()) != null) {
+                res = callback.doSomethingWithLine(line, res);
+            }
+            return res;
         } catch (IOException e) {
             e.printStackTrace();
             throw e;
