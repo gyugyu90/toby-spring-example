6c3303fc73d971c4124a4c3ee1ba4e0ad9ef7495
gyugyu90 <kh2000park@gmail.com>
2019-05-05 15:42:11 +0900

    3.3.1 전략 클래스의 추가 정보

--- src/main/java/user/dao/UserDao.java
+++ src/main/java/user/dao/UserDao.java
@@ -20,21 +20,9 @@ public class UserDao {
     }
 
     public void add(User user) throws SQLException {
-        Connection connection = dataSource.getConnection();
-
-        PreparedStatement ps = connection.prepareStatement("insert into users(id, name, password) values(?,?,?)");
-        ps.setString(1, user.getId());
-        ps.setString(2, user.getName());
-        ps.setString(3, user.getPassword());
-
-        ps.executeUpdate();
-
-        ps.close();
-
-        connection.close();
+        StatementStrategy st = new AddStatement(user);
+        jdbcContextWithStatementStrategy(st);
     }
-
-
 
     public User get(String id) throws SQLException{
 
