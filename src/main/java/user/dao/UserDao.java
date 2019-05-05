e42ea8636d9a1d51395a41549d33dac601c94df9
gyugyu90 <kh2000park@gmail.com>
2019-05-05 15:55:30 +0900

    3.3.2 전략과 클라이언트의 동거 - 로컬 클래스

--- src/main/java/user/dao/UserDao.java
+++ src/main/java/user/dao/UserDao.java
@@ -19,19 +19,13 @@ public class UserDao {
         this.dataSource = dataSource;
     }
 
-    public void add(User user) throws SQLException {
+    public void add(final User user) throws SQLException {
 
         class AddStatement implements StatementStrategy {
-
-            private User user;
-
-            public AddStatement(User user) {
-                this.user = user;
-            }
-
             @Override
             public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
                 PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+                // 로컬 클래스의 코드에서 user 접근 가능
                 ps.setString(1, user.getId());
                 ps.setString(2, user.getName());
                 ps.setString(3, user.getPassword());
@@ -39,8 +33,7 @@ public class UserDao {
             }
         }
 
-
-        StatementStrategy st = new AddStatement(user);
+        StatementStrategy st = new AddStatement();
         jdbcContextWithStatementStrategy(st);
     }
 
