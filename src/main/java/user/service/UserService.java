6e069178f49837b0087e756d90a31910d81087c7
gyugyu90 <kh2000park@gmail.com>
2019-05-07 21:03:44 +0900

    5.1.4 UserService.add()

--- src/main/java/user/service/UserService.java
+++ src/main/java/user/service/UserService.java
@@ -40,5 +40,10 @@ public class UserService {
 
     }
 
+    public void add(User user) {
+        if(user.getLevel() == null) user.setLevel(Level.BASIC);
+        userDao.add(user);
+    }
+
 
 }
--- src/test/java/com/gyugyu/tobyspringexample/UserServiceTest.java
+++ src/test/java/com/gyugyu/tobyspringexample/UserServiceTest.java
@@ -41,6 +41,7 @@ public class UserServiceTest {
         );
     }
 
+
     @Test
     public void upgradeLevels() {
         userDao.deleteAll();
@@ -59,6 +60,26 @@ public class UserServiceTest {
         User userUpdate = userDao.get(user.getId());
         assertThat(userUpdate.getLevel(), is(expectedLevel));
     }
+
+    @Test
+    public void add() {
+        userDao.deleteAll();
+
+        User userWithLevel = users.get(4); // GOLD 레벨
+        User userWithoutLevel = users.get(0);
+        userWithoutLevel.setLevel(null);
+
+        userService.add(userWithLevel);
+        userService.add(userWithoutLevel);
+
+        User userWithLevelRead = userDao.get(userWithLevel.getId());
+        User userWithoutLevelRead = userDao.get(userWithoutLevel.getId());
+
+        assertThat(userWithLevelRead.getLevel(), is(userWithLevel.getLevel()));
+        assertThat(userWithoutLevelRead.getLevel(), is(Level.BASIC));
+
+    }
+
 
 
 }
