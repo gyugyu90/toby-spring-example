5ccd8520b89c380ccc441e39dd6ba4213e34e419
박규혁(Kyuhyeok Park)/상품관리개발팀/11ST <gyugyu@sk.com>
2020-10-16 21:27:20 +0900

    7.2.2 XML 파일을 이용하는 SQL 서비스

---
+++ src/main/java/user/sqlservice/XmlSqlService.java
@@ -0,0 +1,46 @@
+package user.sqlservice;
+
+import user.dao.UserDao;
+import user.sqlservice.jaxb.SqlType;
+import user.sqlservice.jaxb.Sqlmap;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class XmlSqlService implements SqlService{
+
+    private final Map<String, String> sqlMap = new HashMap<>();
+
+    public XmlSqlService() {
+
+        String contextPath = Sqlmap.class.getPackage().getName();
+
+        try {
+            JAXBContext context = JAXBContext.newInstance(contextPath);
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            InputStream is = UserDao.class.getResourceAsStream("/sqlmap-user.xml");
+            Sqlmap sqlmap = (Sqlmap) unmarshaller.unmarshal(is);
+
+            for (SqlType sql: sqlmap.getSql()) {
+                sqlMap.put(sql.getKey(), sql.getValue());
+            }
+        } catch (JAXBException ex) {
+            throw new RuntimeException(ex);
+        }
+
+    }
+
+    @Override
+    public String getSql(String key) throws SqlRetrievalFailureException {
+
+        String sql = sqlMap.get(key);
+        if (sql == null)
+            throw new SqlRetrievalFailureException(key + "를 이용해서 SQL을 찾을 수 없습니다.");
+        else
+            return sql;
+    }
+}
