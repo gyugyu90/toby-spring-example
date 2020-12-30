d9bece9ac1a364963a5a8fc7700cf11cd3ce7954
gyugyu90 <kh2000park@gmail.com>
2020-12-30 15:45:49 +0900

    7.6.2 빈스캐닝과 자동와이어링

--- src/main/java/user/ApplicationContext.java
+++ src/main/java/user/ApplicationContext.java
@@ -1,13 +1,38 @@
 package user;
 
+import com.mysql.jdbc.Driver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = "user")
 @Import(SqlServiceContext.class)
 public class ApplicationContext {
+
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost/testdb?serverTimezone=Asia/Seoul");
+        dataSource.setUsername("root");
+        dataSource.setPassword("vksehr12!@");
+        return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        DataSourceTransactionManager tm = new DataSourceTransactionManager();
+        tm.setDataSource(dataSource());
+        return tm;
+    }
+
 }
--- src/test/java/user/TestApplicationContext.java
+++ src/test/java/user/TestApplicationContext.java
@@ -1,39 +1,16 @@
 package user;
 
-import org.springframework.context.annotation.Profile;
-import user.UserServiceTest.TestUserService;
-import com.mysql.jdbc.Driver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.springframework.context.annotation.Profile;
 import org.springframework.mail.MailSender;
-import org.springframework.transaction.PlatformTransactionManager;
+import user.UserServiceTest.TestUserService;
 import user.service.DummyMailSender;
 import user.service.UserService;
-
-import javax.sql.DataSource;
 
 @Configuration
 @Profile("test")
 public class TestApplicationContext {
-
-    @Bean
-    public DataSource dataSource() {
-        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-        dataSource.setDriverClass(Driver.class);
-        dataSource.setUrl("jdbc:mysql://localhost/testdb?serverTimezone=Asia/Seoul");
-        dataSource.setUsername("root");
-        dataSource.setPassword("vksehr12!@");
-        return dataSource;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        DataSourceTransactionManager tm = new DataSourceTransactionManager();
-        tm.setDataSource(dataSource());
-        return tm;
-    }
 
     @Bean
     public UserService testUserService() {
