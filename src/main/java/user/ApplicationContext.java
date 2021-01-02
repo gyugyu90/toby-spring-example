b1a82e9af81b42df5f2860cb4ca0599a691c8f67
gyugyu90 <kh2000park@gmail.com>
2021-01-02 16:25:53 +0900

    7.6.6 빈 설정의 재사용과 @Enable*

--- src/main/java/user/ApplicationContext.java
+++ src/main/java/user/ApplicationContext.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -21,8 +20,8 @@ import javax.sql.DataSource;
 @Configuration
 @EnableTransactionManagement
 @ComponentScan(basePackages = "user")
-@Import(SqlServiceContext.class)
 @PropertySource("/database.properties")
+@EnableSqlService
 public class ApplicationContext implements SqlMapConfig {
 
     @Value("${db.driverClass}")
---
+++ src/main/java/user/EnableSqlService.java
@@ -0,0 +1,7 @@
+package user;
+
+import org.springframework.context.annotation.Import;
+
+@Import(value = SqlServiceContext.class)
+public @interface EnableSqlService {
+}
