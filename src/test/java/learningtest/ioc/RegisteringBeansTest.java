0b121fa477739e2d61ebdc79a4fbc074c66227dd
gyugyu90 <kh2000park@gmail.com>
2021-01-20 20:48:53 +0900

    1.3.1 프로토타입 스코프

--- src/test/java/learningtest/ioc/RegisteringBeansTest.java
+++ src/test/java/learningtest/ioc/RegisteringBeansTest.java
@@ -2,15 +2,21 @@ package learningtest.ioc;
 
 import learningtest.ioc.bean.AnnotatedHello;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
 import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.context.support.StaticApplicationContext;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.*;
@@ -104,5 +110,58 @@ public class RegisteringBeansTest {
 
         AnnotatedHelloConfig config = context.getBean("annotatedHelloConfig", AnnotatedHelloConfig.class);
         assertThat(config, is(notNullValue()));
+    }
+
+    @Test
+    public void singletonScope() {
+
+        ApplicationContext context = new AnnotationConfigApplicationContext(SingletonBean.class, SingletonClientBean.class);
+
+        Set<SingletonBean> beans = new HashSet<>(); // set은 중복을 허용하지 않으므로 같은 오브젝트는 여러 번 추가해도 한 개만 남는다.
+
+        beans.add(context.getBean(SingletonBean.class));
+        beans.add(context.getBean(SingletonBean.class)); // DL에서 싱글톤 확인
+        assertThat(beans.size(), is(1));
+
+        beans.add(context.getBean(SingletonClientBean.class).bean1);
+        beans.add(context.getBean(SingletonClientBean.class).bean2); // DI에서 싱글톤 확인
+        assertThat(beans.size(), is(1));
+
+    }
+
+    static class SingletonBean {}
+
+    static class SingletonClientBean {
+        @Autowired SingletonBean bean1;
+        @Autowired SingletonBean bean2;
+    }
+
+    @Test
+    public void prototypeScope() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(
+                PrototypeBean.class,
+                PrototypeClientBean.class
+        );
+
+        Set<PrototypeBean> bean = new HashSet<>();
+
+        bean.add(context.getBean(PrototypeBean.class));
+        assertThat(bean.size(), is(1));
+        bean.add(context.getBean(PrototypeBean.class));
+        assertThat(bean.size(), is(2));
+
+        bean.add(context.getBean(PrototypeClientBean.class).bean1);
+        assertThat(bean.size(), is(3));
+        bean.add(context.getBean(PrototypeClientBean.class).bean2);
+        assertThat(bean.size(), is(4));
+
+    }
+
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    static class PrototypeBean {}
+
+    static class PrototypeClientBean {
+        @Autowired PrototypeBean bean1;
+        @Autowired PrototypeBean bean2;
     }
 }
