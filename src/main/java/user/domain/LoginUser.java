805fe2415bf1e4f21ae8d28bf938c88b083f80ba
gyugyu90 <kh2000park@gmail.com>
2021-01-31 10:39:15 +0900

    1.3.2 세션 스코프 - DI 적용

--- src/main/java/user/domain/LoginUser.java
+++ src/main/java/user/domain/LoginUser.java
@@ -2,10 +2,11 @@ package user.domain;
 
 import lombok.Data;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 
 import java.util.Date;
 
-@Scope("session")
+@Scope(value = "session", proxyMode = ScopedProxyMode.TARGET_CLASS)
 @Data
 public class LoginUser {
     private String loginId;
--- src/main/java/user/service/LoginService.java
+++ src/main/java/user/service/LoginService.java
@@ -3,20 +3,17 @@ package user.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import user.domain.LoginUser;
 
-import javax.inject.Provider;
 import java.util.Date;
 
 public class LoginService {
 
     @Autowired
-    Provider<LoginUser> loginUserProvider;
+    LoginUser loginUser; // 스코프에 따라서 다른 오브젝트로 연결되는 프록시가 주입된다.
 
     public void login(Login login) {
-        LoginUser loginUser = loginUserProvider.get();
         loginUser.setLoginId(login.id);
         loginUser.setLoginTime(new Date());
     }
-
 
     static class Login {
         String id;
